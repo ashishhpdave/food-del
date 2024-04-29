4544156c00f6ccb802bc60687a0fdb263a535e62
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2024-04-29 22:54:40 +0530

    Header-Create

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Choose From a diverse menu featuring a delectable crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experie</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
