58c9ad6a1596119f3d1cb3532a27608c1ab05ad8
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2024-04-29 14:24:10 +0530

    Navbar-Create
    This Project Navbar Created....

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+        <div className="app">
+          <Navbar />
+        </div>
+    </div>
+  )
+}
+
+export default App
