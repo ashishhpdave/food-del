9dcb12f0a78f533bb8670e32a9fb61064c67b868
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2024-05-07 16:57:32 +0530

    Login-Message-Show
    This is message show

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,6 +6,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
+    const url = "http://localhost:4000"
+    const [token,setToken] = useState("");
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
@@ -37,7 +39,10 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
 
     return (
