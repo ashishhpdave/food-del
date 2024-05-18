6cf450b3dd2e4a1064aaa2e01084fe934f09b9f5
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2024-05-18 22:02:57 +0530

    RefreshPageErrorSolve
    This is the code for page will be refresh then product cart will be automaticly reset and this is code it fix it

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,7 +21,7 @@ const loginUser = async (req,res) => {
     const token = createToken(user._id);
     res.json({success:true,token})
 
-   } catch (error) {
+   } catch (err3or) {
     console.log(error);
     res.json({success:false,message:"Error"});
    }
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -10,17 +10,23 @@ const StoreContextProvider = (props) => {
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([])
 
-    const addToCart = (itemId) => {
+    const addToCart = async (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
         }
         else {
             setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
+        if (token) {
+            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+        }
     }
 
-    const removeFromCart = (itemId) => {
+    const removeFromCart = async (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+        if (token) {
+            await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+        }
     }
 
     const getTotalCartAmount = () => {
@@ -39,12 +45,18 @@ const StoreContextProvider = (props) => {
         setFoodList(response.data.data)
     }
 
+    const loadCarData = async (token) =>{
+        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}});
+        setCartItems(response.data.cartData);
+    }
+
     useEffect(()=>{
        
        async function loadData(){
         await fetchFoodList()
         if (localStorage.getItem("token")) {
             setToken(localStorage.getItem("token"));
+            await loadCarData(localStorage.getItem("token"))
           }
        }
        loadData();
